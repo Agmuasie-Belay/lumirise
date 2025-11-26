1be39c76714460d868b29a977e920e9b7ab25366
Agmuasie Belay <agmuasie.b@gmail.com>
2025-11-27 02:56:52 +0300

    Unresponsive redirecting fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,11 +7,7 @@ export const connectDB = async () => {
   const MONGO_URI = process.env.MONGO_URI;
   try {
     await mongoose.connect(MONGO_URI);
-<<<<<<< HEAD
     console.log("MongoDB connected: learntrack DB");
-=======
-    console.log("MongoDB connected");
->>>>>>> 3dcaf8113fbb4d94778dc562e219ad6e5ca7d9e3
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
